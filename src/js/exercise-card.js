cb138be4276f3c9f36f34e89e1adb3592480e615
Veronika Galliamova <vrnkgllmvaa@gmail.com>
2026-04-27 23:49:09 +0200

    feat: implement exercise card component and add pagination to the favorites page

---
+++ src/js/exercise-card.js
@@ -0,0 +1,58 @@
+export function createExerciseCardMarkup(exercise, isFavoritePage = true) {
+  const { _id, name, burnedCalories, time, bodyPart, target, rating } =
+    exercise;
+
+  const topActionMarkup = isFavoritePage
+    ? `<button class="exercise-card-trash-btn" type="button" aria-label="Remove from favorites">
+         <svg width="16" height="16">
+           <use href="./img/sprite.svg#icon-trash"></use>
+         </svg>
+       </button>`
+    : `<div class="exercise-card-rating">
+         <span class="exercise-card-rating-value">${rating ? Math.round(rating * 10) / 10 : '0.0'}</span>
+         <svg class="exercise-card-rating-icon" width="18" height="18">
+           <use href="./img/sprite.svg#icon-star"></use>
+         </svg>
+       </div>`;
+
+  return `
+    <li class="exercise-card" data-id="${_id}">
+      <div class="exercise-card-header">
+        <div class="exercise-card-badge-wrap">
+          <span class="exercise-card-badge">WORKOUT</span>
+          ${topActionMarkup}
+        </div>
+        <button class="exercise-card-start-btn" type="button">
+          Start
+          <svg width="16" height="16">
+            <use href="./img/sprite.svg#icon-arrow"></use>
+          </svg>
+        </button>
+      </div>
+      
+      <div class="exercise-card-title-wrap">
+        <div class="exercise-card-icon-wrap">
+          <svg class="exercise-card-icon" width="24" height="24">
+            <use href="./img/sprite.svg#icon-running-man"></use>
+          </svg>
+        </div>
+        <h3 class="exercise-card-title">${name}</h3>
+      </div>
+      
+      <ul class="exercise-card-info-list">
+        <li class="exercise-card-info-item">
+          <span class="exercise-card-info-label">Burned calories:</span>
+          <span class="exercise-card-info-value">${burnedCalories} / ${time} min</span>
+        </li>
+        <li class="exercise-card-info-item">
+          <span class="exercise-card-info-label">Body part:</span>
+          <span class="exercise-card-info-value">${bodyPart}</span>
+        </li>
+        <li class="exercise-card-info-item">
+          <span class="exercise-card-info-label">Target:</span>
+          <span class="exercise-card-info-value">${target}</span>
+        </li>
+      </ul>
+    </li>
+  `;
+}
